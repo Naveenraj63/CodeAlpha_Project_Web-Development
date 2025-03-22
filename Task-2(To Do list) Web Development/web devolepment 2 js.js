4b6500066fb693297df7230570986a2b9ab42699
Naveenraj63 <snaveenraj6374@gmail.com>
2025-03-22 09:31:02 +0530

    Add files via upload

---
+++ Task-2(To Do list) Web Development/web devolepment 2 js.js
@@ -0,0 +1,26 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+    deleteBtn.onclick = function () {
+        taskList.removeChild(li);
+    };
+
+    li.onclick = function () {
+        li.classList.toggle("completed");
+    };
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
